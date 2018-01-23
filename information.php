ba74a202d1fbbf2a7d790437cc50029dd54e222a
tatatan <32958443+tatatan@users.noreply.github.com>
2018-01-24 02:03:11 +0700

    add information page

---
+++ information.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php 
+include('navbar.php');
+?>
+
+<html>
+<head><title> information </title></head>
+  <div class="container">
+    
+    <h5 class = "text-info"> หาปุ่มไม่ถูก/สมัครไม่เป็น? </h5>
+    <p> 1. กด Sign Up ก่อน (ในมือถือ : ให้กดตรงมุมขวาบน) </p>
+    <p> 2. พอ register เว็บเสร็จแล้ว ต้อง login อีกรอบถึงจะเข้าเว็บได้ </p>
+    <p> 3. ถ้ายังไม่ได้ลองติดต่อฝ่าย OD มาค่า :D </p>
+  </div>
+  <div class = "container">
+    <p> </p>
+    <h5 class = "text-info"> สมัครด้วย facebook ไม่ได้? </h5>
+    <p> 1. ลองเปลี่ยน browser ดูก่อน แนะนำเป็น google chrome/safari </p>
+    <p> 2. tbc (อยู่ในช่วงทดลองใช้ แฮ่ๆ)
+    <h1>  </h1>
+    
+    <h5 class = "text-info"> รูปประกอบวิธีเข้าเว็บ/สมัครสมาชิก </h5>
+    <p> รูปประกอบสำหรับใช้ในมือถือนะคะ </p>
+    <img src = "img/info1.png"  class="img-responsive" style="max-width=100px"></img>
+    <img src = "img/info2.png"  class="img-responsive"></img>
+    <img src = "img/info3.png"  class="img-responsive"></img>
+
+  </div>
+</html>
