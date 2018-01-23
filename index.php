1cb7e61963b4887293414ea94fb5ef115e5f854d
tatatan <32958443+tatatan@users.noreply.github.com>
2018-01-24 02:16:29 +0700

    Updating homepage

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
-  header('Location: login.php')
+<!DOCTYPE html>
+<?php 
+include('navbar.php');
 ?>
+
+<head><title> STEPS </title></head>
+<html>
+  <div class="container">
+    <div>
+    <h4 class= "text-info"> Welcome to steps website </h4>
+    <p> เว็บอยู่ในช่วงทดลองใช้ ใจเย็นเข้าไว้เด้อ55 </p>
+    <br />
+    <br />    
+    <h5 class = "text-info"> เข้าเว็บครั้งแรก ลงทะเบียนก่อน </h5>
+    <p><a href="regis.php"> Sign Up </a></p>
+    <br />
+
+    <h5 class = "text-info"> ลงทะเบียนแล้ว login เข้าใช้งาน</h5>
+    <p><a href="information.php"> Log In </a></p>
+    
+    <br />
+    <h5 class = "text-info"> มีปัญหาการใช้งาน / งง </h5>
+    <p><a href="information.php"> info </a></p>
+    <br />
+
+  </div>
+</html>
